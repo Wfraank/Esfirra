d878d490ed21ad4f8b5cda06eae0e43658bd394c
Andreza Dantas Layme Pifano de Moura <adlpm@uhwc03.windows.cin.ufpe.br>
2018-07-07 15:08:13 -0300

    edição do models.py relacionados a nossa tabela

--- aotorcedor/aotorcedor/models.py
+++ aotorcedor/aotorcedor/models.py
@@ -5,12 +5,42 @@ from django.db import models
 
 class Torcedor(models.Model):
 
+    ID = models.CharField(
+        max_length=60,
+        null=False,
+        blank=False
+    )
+
+    email = models.CharField(
+        max_length=60,
+        null=False,
+        blank=False
+    )
+ 
+    senha = models.CharField(
+        max_length=25,
+        null=False,
+        blank=False
+    )
+ 
     nome = models.CharField(
         max_length=255,
         null=False,
         blank=False
     )
 
+    cidade = models.CharField(
+        max_length=25,
+        null=False,
+        blank=False
+    )
+
+    estado = models.CharField(
+        max_length=2,
+        null=False,
+        blank=False
+    )
+ 
     cpf = models.CharField(
         max_length=14,
         null=False,
@@ -31,9 +61,21 @@ class Torcedor(models.Model):
     )
 
     nacionalidade = models.CharField(
-        max_length=3,
+        max_length=25,
         null=False,
         blank=False
     )
 
+    estado_civil = models.CharField(
+        max_length=25,
+        null=False,
+        blank=False
+    )
+ 
+    pontuacao = models.IntegerField(
+        default=1000,
+        null=False,
+        blank=False
+    )
+ 
     objetos = models.Manager()
