74c0536e37c8064498f131b00544ac28c0d341e2
Jose Raphael Danillo dos Santos <jrds@WINDOWS.CIN.UFPE.BR>
2018-07-07 15:59:56 -0300

    Atualiza codigo arduino

--- arduino/sensor/sensor.ino
+++ arduino/sensor/sensor.ino
@@ -15,26 +15,171 @@
 
 #include <KNoTThing.h>
 #include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
 
 
-#define LIGHT_BULB_PIN      2
-#define LIGHT_BULB_ID       1
-#define LIGHT_BULB_NAME     "Light bulb"
+#define SENSOR_BIOMETRIC_PIN1      2
+#define SENSOR_BIOMETRIC_PIN2      3
+#define SENSOR_BIOMETRIC_ID        1
+#define SENSOR_BIOMETRIC_NAME      "Biometria"
+
+SoftwareSerial mySerial(SENSOR_BIOMETRIC_PIN1, SENSOR_BIOMETRIC_PIN2);
 
 KNoTThing thing;
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 
-static int light_read(uint8_t *val)
-{
-    *val = digitalRead(LIGHT_BULB_PIN);
-    Serial.print(F("Light Status: "));
-    if (*val)
-      Serial.println(F("ON"));
-    else
-      Serial.println(F("OFF"));
-    return 0;
+uint8_t id;
+
+uint8_t readnumber(void) {
+  uint8_t num = 0;
+  
+  while (num == 0) {
+    while (! Serial.available());
+    num = Serial.parseInt();
+  }
+  return num;
 }
 
-static int light_write(uint8_t *val)
+uint8_t getFingerprintEnroll() {
+
+  int p = -1;
+  Serial.print("Waiting for valid finger to enroll as #"); Serial.println(id);
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(1);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  Serial.println("Remove finger");
+  delay(2000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+  Serial.print("ID "); Serial.println(id);
+  p = -1;
+  Serial.println("Place same finger again");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.print(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  // OK success!
+
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  Serial.print("Creating model for #");  Serial.println(id);
+  
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Prints matched!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_ENROLLMISMATCH) {
+    Serial.println("Fingerprints did not match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  Serial.print("ID "); Serial.println(id);
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Stored!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not store in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+}
+
+/*static int light_write(uint8_t *val)
 {
     digitalWrite(LIGHT_BULB_PIN, *val);
     Serial.print(F("Light Status: "));
@@ -42,24 +187,40 @@ static int light_write(uint8_t *val)
       Serial.println(F("ON"));
     else
       Serial.println(F("OFF"));
-      /* TODO: Save light status in EEMPROM in to handle when reboot */
+      /* TODO: Save light status in EEMPROM in to handle when reboot 
     return 0;
-}
+}*/
 
 void setup()
 {
     Serial.begin(9600);
-
-    pinMode(LIGHT_BULB_PIN, OUTPUT);
+    while (!Serial);  
+    delay(100);
+    finger.begin(57600);
+    if (finger.verifyPassword()) {
+      Serial.println("Found fingerprint sensor!");
+    } else {
+      Serial.println("Did not find fingerprint sensor :(");
+      while (1) { delay(1); }
+    }
+    Serial.println("Ready to enroll a fingerprint!");
+    Serial.println("Please type in the ID # (from 1 to 127) you want to save this finger as...");
+    id = readnumber();
+    if (id == 0) {// ID #0 not allowed, try again!
+       return;
+    }
+    Serial.print("Enrolling ID #");
+    Serial.println(id);
+    
     /* TODO: Read lamp status from eeprom for reboot cases */
-    thing.init("KNoTThing");
-    thing.registerBoolData(LIGHT_BULB_NAME, LIGHT_BULB_ID, KNOT_TYPE_ID_SWITCH,
-        KNOT_UNIT_NOT_APPLICABLE, light_read, light_write);
+    thing.init("AoTorcedor_SB");
+    thing.registerIntData(SENSOR_BIOMETRIC_NAME, SENSOR_BIOMETRIC_ID, KNOT_TYPE_ID_NONE,
+        KNOT_UNIT_NOT_APPLICABLE, getFingerprintEnroll, NULL);
 
     /* Send data every 10 seconds*/
-    thing.registerDefaultConfig(LIGHT_BULB_ID, KNOT_EVT_FLAG_TIME, 10, 0, 0, 0, 0);
+    thing.registerDefaultConfig(SENSOR_BIOMETRIC_ID, KNOT_EVT_FLAG_CHANGE, 0, 0, 0, 0, 0);
 
-    Serial.println(F("Remote Light Bulb KNoT Demo"));
+    Serial.println(F("Remote Biometria KNoT Demo"));
 }
 
 
