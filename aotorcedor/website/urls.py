d0e1658f3105c1b24d9901b4ffdb4f7513253a37
wfrank <fraanklincordeiro@gmail.com>
2018-07-09 04:03:16 -0300

    Implementação das views

--- aotorcedor/website/urls.py
+++ aotorcedor/website/urls.py
@@ -1,12 +1,18 @@
 
+from django.contrib.auth import views as auth_views
+from website.views import IndexTemplateView, TorcedorCreateView, user_login
 from django.urls import path
-from website.views import IndexTemplateView
+from . import views
+
 
 app_name = 'website'
 
 # urlpatterns a lista de roteamentos de URLs para funções/Views
 urlpatterns = [
     # GET /
-    path('', IndexTemplateView.as_view()),
+    path('', IndexTemplateView.as_view(), name="index"),
+    path('torcedor/cadastrar/', TorcedorCreateView.as_view(), name="cadastro_torcedor"),
+    path('torcedor/login/', auth_views.login, name="login"),
+
 ]
 
--- aotorcedor/website/views.py
+++ aotorcedor/website/views.py
@@ -1,11 +1,48 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+from aotorcedor.models import Torcedor
+from website.forms import InsereTorcedor, LoginForm
+
 
 # Create your views here.
 
 
-
-
 class IndexTemplateView(TemplateView):
   template_name = "website/index.html"
+
+
+
+class TorcedorCreateView(CreateView):
+  template_name = "website/cadastro.html"
+  model = Torcedor
+  form_class = InsereTorcedor
+  success_url =  reverse_lazy("website:index")
+
+
+def user_login(request):
+  if request.method == 'POST':
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      cd = form.cleaned_data
+      user = authenticate(username=cd['username'],password=cd['password'])
+      if user is not None:
+        if user .is_active:
+          login(request, user)
+          return HttpResponse('Authenticated successfully')
+        else:
+          return HttpResponse('Disabled account')
+
+      else:
+        return HttpResponse('Invalid Login')
+
+    else:
+      form = LoginForm()
+
+    return render(request, "website/login.html", {'form': form})
+
+
 
